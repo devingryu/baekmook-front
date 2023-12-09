99e5aa39fd21378f1042d636bcd37671f931a3fa
Fluotin Halog <devfluotin@gmail.com>
2023-12-09 17:21:33 +0900

    ftr: implemented drawer

--- app/routes/_sidebar/route.tsx
+++ app/routes/_sidebar/route.tsx
@@ -19,29 +19,46 @@ import {
   Popover,
   Divider,
   useMediaQuery,
+  Drawer,
 } from "@mui/material";
 import { type OverridableComponent } from "@mui/material/OverridableComponent";
 import { useAuth } from "~/utils/util";
 import { useState } from "react";
-import { STRING_BAEKMOOK, STRING_DASHBOARD, STRING_LECTURE, STRING_LOGIN, STRING_LOGOUT, STRING_MYPAGE } from "~/resources/strings";
+import {
+  STRING_BAEKMOOK,
+  STRING_DASHBOARD,
+  STRING_LECTURE,
+  STRING_LOGIN,
+  STRING_LOGOUT,
+  STRING_MYPAGE,
+} from "~/resources/strings";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: sidebarCss },
 ];
 
-type SidebarItemProps = {
-  Icon: OverridableComponent<SvgIconTypeMap<{}, "svg">>;
+interface SidebarItemProps {
+  icon: OverridableComponent<SvgIconTypeMap<{}, "svg">>;
   linkTo: string;
   isSelected: boolean;
   caption: string;
-};
+}
+
+interface DrawerItemProps extends SidebarItemProps {
+  handleDrawerClose: () => void;
+}
+
+interface SidebarProps {
+  items: SidebarItemProps[];
+}
 
 interface TopBarProps {
-  isMobile: boolean
+  isMobile: boolean;
+  handleDrawerOpen: () => void;
 }
 
 const SidebarItem = ({
-  Icon,
+  icon: Icon,
   linkTo,
   isSelected,
   caption,
@@ -105,10 +122,68 @@ const SidebarItem = ({
   );
 };
 
-const Sidebar = () => {
-  const loc = useLocation();
-  const auth = useAuth();
-  
+const DrawerItem = ({
+  icon: Icon,
+  linkTo,
+  isSelected,
+  caption,
+  handleDrawerClose,
+}: DrawerItemProps) => {
+  const theme = useTheme();
+  return (
+    <Stack
+      component={Link}
+      to={linkTo}
+      onClick={handleDrawerClose}
+      direction="row"
+      alignItems="center"
+      sx={[
+        {
+          borderRadius: 2,
+          transitionDuration: "0.2s",
+          color: "inherit",
+          textDecoration: "inherit",
+          backgroundColor: isSelected
+            ? theme.colors.m3.secondaryContainer
+            : theme.colors.m3.surface,
+          p: "8px 12px",
+          minWidth: "200px",
+        },
+        !isSelected && {
+          "@media (pointer: fine)": {
+            ":hover": {
+              backgroundColor: theme.palette.action.hover,
+            },
+          },
+          ":active": {
+            backgroundColor: theme.palette.action.selected,
+          },
+        },
+      ]}
+    >
+      <Icon
+        sx={{
+          display: "block",
+          width: "24px",
+          height: "24px",
+          margin: "2px",
+          color: isSelected
+            ? theme.colors.m3.onSurfaceVariant
+            : theme.colors.m3.onSecondaryContainer,
+        }}
+      />
+      <Typography
+        variant="body1"
+        textAlign="center"
+        sx={{ userSelect: "none", ml: 3 }}
+      >
+        {caption}
+      </Typography>
+    </Stack>
+  );
+};
+
+const Sidebar = ({ items }: SidebarProps) => {
   return (
     <div className="sidenav">
       <Stack
@@ -118,28 +193,19 @@ const Sidebar = () => {
         direction="column"
       >
         <Box height="3px" />
-        <SidebarItem
-          Icon={DashboardIcon}
-          linkTo="/home"
-          isSelected={loc.pathname === "/home"}
-          caption={STRING_DASHBOARD}
-        />
-        {auth && <SidebarItem
-          Icon={HistoryEduIcon}
-          linkTo="/lectures"
-          isSelected={loc.pathname.startsWith('/lectures')}
-          caption={STRING_LECTURE}
-        />}
+        {items.map((it) => (
+          <SidebarItem key={it.caption} {...it} />
+        ))}
       </Stack>
     </div>
   );
 };
-const TopBar = ({isMobile}: TopBarProps) => {
-  const [anchorEl, setAnchorEl] = useState<HTMLImageElement | null>(null);
-  const open = Boolean(anchorEl);
+const TopBar = ({ isMobile, handleDrawerOpen }: TopBarProps) => {
   const auth = useAuth();
   const submit = useSubmit();
   const theme = useTheme();
+  const [anchorEl, setAnchorEl] = useState<HTMLImageElement | null>(null);
+  const open = Boolean(anchorEl);
 
   const handleProfileClick = (event: React.MouseEvent<HTMLImageElement>) => {
     setAnchorEl(event.currentTarget);
@@ -167,9 +233,20 @@ const TopBar = ({isMobile}: TopBarProps) => {
         opacity: 0.9,
       }}
     >
-      <IconButton sx={{ width: "36px", height: "36px", margin: isMobile ? "0 8px 0 12px" : "0 18px" }}>
-        <MenuIcon />
-      </IconButton>
+      {isMobile ? (
+        <IconButton
+          onClick={handleDrawerOpen}
+          sx={{
+            width: "36px",
+            height: "36px",
+            margin: "0 8px 0 12px",
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+      ) : (
+        <Box width="20px" />
+      )}
       <ModeIcon sx={{ height: "36px", width: "36px" }} />
       <Typography
         variant="subtitle1"
@@ -272,12 +349,53 @@ const TopBar = ({isMobile}: TopBarProps) => {
   );
 };
 const Index = () => {
-  const theme = useTheme()
+  const theme = useTheme();
+  const loc = useLocation();
+  const auth = useAuth();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const handleDrawerOpen = () => setIsDrawerOpen(true);
+  const handleDrawerClose = () => setIsDrawerOpen(false);
+
+  const sidebarItems = [
+    {
+      icon: DashboardIcon,
+      linkTo: "/home",
+      isSelected: loc.pathname === "/home",
+      caption: STRING_DASHBOARD,
+    },
+    ...(auth
+      ? [
+          {
+            icon: HistoryEduIcon,
+            linkTo: "/lectures",
+            isSelected: loc.pathname.startsWith("/lectures"),
+            caption: STRING_LECTURE,
+          },
+        ]
+      : []),
+  ];
   return (
     <>
-      {!isMobile && <Sidebar />}
-      <TopBar isMobile={isMobile}/>
+      {!isMobile && <Sidebar items={sidebarItems} />}
+      <TopBar isMobile={isMobile} handleDrawerOpen={handleDrawerOpen} />
+      <Drawer
+        anchor="left"
+        open={isDrawerOpen && isMobile}
+        onClose={handleDrawerClose}
+      >
+        <Box sx={{ diplay: "flex", direction: "column", padding: "16px 8px" }}>
+          {sidebarItems.map((it) => (
+            <DrawerItem
+              key={it.caption}
+              handleDrawerClose={handleDrawerClose}
+              {...it}
+            />
+          ))}
+        </Box>
+      </Drawer>
       <div className="content" style={{ marginLeft: isMobile ? 0 : "72px" }}>
         <Box sx={{ maxWidth: "1200px", margin: "0 auto" }}>
           <Outlet />
